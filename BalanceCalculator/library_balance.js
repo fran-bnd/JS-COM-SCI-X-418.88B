ab0fcae936c51a3261cdf91bf6e048d3269a4590
fran-bnd <104868117+fran-bnd@users.noreply.github.com>
2023-05-17 20:50:21 -0300

    fixed todays date with padStart(2, '0')

--- BalanceCalculator/library_balance.js
+++ BalanceCalculator/library_balance.js
@@ -12,6 +12,7 @@ var getTransaction = function(index) {
 
 var addTransaction = function (type, amount, date) {
    
+    // aux to display the ampunt correctly on the table
     var amountDisplay = "";
     if ( type == "deposit"){
         amountDisplay = amount;
@@ -19,8 +20,19 @@ var addTransaction = function (type, amount, date) {
         amountDisplay = "("+amount+")";
     }
 
+    // aux to display the date correctly on the table
+    var dateDisplay = new Date();
+    if ( date == null ){
+        var year = dateDisplay.getFullYear();
+        var month = String(dateDisplay.getMonth() + 1).padStart(2, '0');
+        var day = String(dateDisplay.getDate()).padStart(2, '0');
+        dateDisplay =  year + "-" + month + "-" + day;
+    } else {
+        dateDisplay = date;
+    }
+
     // add object to array of transList
-    transList.push({ "type": type, "amount": amount, "amountDisplay": amountDisplay, "dateDisplay": date });
+    transList.push({ "type": type, "amount": amount, "amountDisplay": amountDisplay, "dateDisplay": dateDisplay });
 
 };
 
@@ -39,16 +51,14 @@ And an onload event handler that attaches the add function to the click event of
 
 In the library_balance.js file, note that an empty array and the start of three functions are supplied.
 
-Review the updateDisplay function in the balance.js file. Note that it calls the getTransaction function of the library file twice. 
+In the updateDisplay function in the balance.js file, note that it calls the getTransaction function of the library file twice. 
 In the first call, no argument is passed and the total number of transactions is returned. 
 In the second call, the value of the loop’s index is passed and a specific transaction is returned. 
-Now, code the getTransaction function in the library file.
 
 Note that the updateDisplay function also calls the calculateBalance function of the library file. 
 This code passes the values it receives from the getTransaction function, and the current balance is returned. 
-Now, code the calculateBalance function in the library file.
 
-Review the add function in the balance.js file. Note that it calls the addTransaction function of the library file, passes it values entered by the user, and doesn’t receive any value in return. 
+In the add function in the balance.js file, note that it calls the addTransaction function of the library file, passes it values entered by the user, and doesn’t receive any value in return. 
 Also note that this function can accept variable numbers of arguments, depending on whether the user entered a value for the date. Now, code the addTransaction function in the library file. 
 Assume that it will use today’s date if it isn’t passed a date.
 */
