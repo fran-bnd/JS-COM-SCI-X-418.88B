9c05998929000b7bf341f42cc805074e82d9ba0a
fran-bnd <104868117+fran-bnd@users.noreply.github.com>
2023-05-31 23:50:52 -0300

    displaying 12h with two digits and AM/PM

--- Clock/clock.js
+++ Clock/clock.js
@@ -2,7 +2,22 @@
 var $ = function(id) { return document.getElementById(id); };
 
 var displayCurrentTime = function() {
-
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    let currentSecond = currentTime.getSeconds();
+    
+    // Convert values to a 12 hour clock and determine the AM/PM value
+    let ampm = currentHour >= 12 ? 'PM' : 'AM';
+    if (currentHour > 12) {
+        currentHour = currentHour - 12;
+    }
+    
+    // Display the values with two digits in the span tags
+    $("hours").textContent = padSingleDigit(currentHour);
+    $("minutes").textContent = padSingleDigit(currentMinute);
+    $("seconds").textContent = padSingleDigit(currentSecond); 
+    $("ampm").textContent = ampm; 
 };
 
 var padSingleDigit = function(num) {
@@ -10,7 +25,20 @@ var padSingleDigit = function(num) {
 };
 
 window.onload = function() {
-    // set initial clock display and then set interval timer to display
-    // new time every second. Don't store timer object because it 
-    // won't be needed - clock will just run.
+    // Set initial clock display 
+    displayCurrentTime();
+    // Set interval timer to display new time every second (1000 miliseconds)
+    let timer;
+    timer = setInterval(displayCurrentTime, 1000);
 };
+
+/*
+In the JavaScript file, note that four functions are supplied. The $ function. The start of a displayCurrentTime function. 
+The padSingleDigit function that adds a leading zero to single digits. And the start of an onload event handler.
+
+In the displayCurrentTime function, code uses the Date object to determine the current hour, minute, and second. 
+Converting these values to a 12 hour clock, determines the AM/PM value, and displays these values in the appropriate span tags.
+
+In the onload event handler, a timer calls the displayCurrentTime function at 1 second intervals. 
+The current time shows as soon as the page loads.
+*/
