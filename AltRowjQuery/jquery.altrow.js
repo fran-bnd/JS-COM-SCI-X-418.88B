9728a272d8aa38f5170052d99c42359be442fc1c
fran-bnd <104868117+fran-bnd@users.noreply.github.com>
2023-06-22 17:34:16 -0300

    jquery working

--- AltRowjQuery/jquery.altrow.js
+++ AltRowjQuery/jquery.altrow.js
@@ -1,1 +1,31 @@
 "use strict";
+
+(function($) {
+    $.fn.altrow = function() {
+      //geting all "tr" child elements
+      var rows = this.find('tr');
+  
+      //adding "header" class to rows with "th" elements
+      rows.filter(function() {
+        return $(this).find('th').length > 0;
+      }).addClass('header');
+  
+      //adding "even" and "odd" classes to the rest of the rows
+      rows.each(function(index) {
+        if (index > 0) {
+          $(this).addClass(index % 2 === 0 ? 'even' : 'odd');
+        }
+      });
+  
+      return this;
+    };
+  })(jQuery);
+
+/*
+In the jquery.altrow.js file, a plugin uses the getElementsByTagName method to get all of an element’s “tr” child elements. 
+
+It checks whether a row contains “th” elements to see if it’s a header row.
+
+Then, applies a class named “header” to the header row, 
+and classes named “even” and “odd” on an alternating basis to the rest of the rows. 
+*/
