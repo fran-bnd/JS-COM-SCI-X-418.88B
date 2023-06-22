e74f7c37fa50821908e275f2c9324f8b629bb76e
fran-bnd <104868117+fran-bnd@users.noreply.github.com>
2023-06-22 17:35:56 -0300

    alt row

--- AltRowjQuery/altrow.js
+++ AltRowjQuery/altrow.js
@@ -1,5 +1,15 @@
 "use strict";
 
 window.onload = function() {
-    
+    $(document).ready(function(){
+        $('#important').altrow();
+    }); //end ready
 };
+
+/*
+In the HTML file, note the script tags for the jQuery library and the plugin file (jquery.altrow.js).
+
+In the altrow.js file, the code onload event calls the plugin for the table element. 
+
+Run the application and see how the row styles are applied to the table.
+*/
